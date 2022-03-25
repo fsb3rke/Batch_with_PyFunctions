6d82bf2c42b08c1b74469943a5a1803c778334be
Berke <69055194+Fsb3rKe@users.noreply.github.com>
2022-03-25 19:25:19 +0300

    Add files via upload

---
+++ src/batpyf.py
@@ -0,0 +1,33 @@
+class Batch:
+    def __init__(self, FileName:str):
+        self.filename:str = FileName
+        self.__clear()
+        self.__create()
+
+    def __splittedFileName(self):
+        return self.filename.split('.')[0] + ".bat"
+
+    def __clear(self):
+        newFileName:str = self.__splittedFileName()
+        open(newFileName, "w")
+
+    def __create(self):
+        newFileName:str = self.__splittedFileName()
+        try:
+            open(newFileName, "x").close()
+            print(f"{newFileName} has been created")
+        except:
+            print(f"{newFileName} has not been created")
+
+    def __appendFile(self, content:str):
+        newFileName:str = self.__splittedFileName()
+        f = open(newFileName, "a")
+        f.write(content+"\n")
+        f.close()
+
+    def cvar(self, variableName:str, content):
+        self.__appendFile(f"SET /A {variableName} = {str(content)}")
+        return f"%{variableName}%"
+
+    def cprint(self, cvar:str):
+        self.__appendFile(f"echo {cvar}")
